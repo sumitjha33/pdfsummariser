f15f7852fe6b3a40cb8176b3f2fde2a95d88a23e
sumitjha33 <sumitjha3333@gmail.com>
2025-03-22 12:05:01 +0530

    Initial commit with project files

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn wsgi:app --log-file -
+web: gunicorn wsgi:app --bind 0.0.0.0:$PORT --timeout 120 --log-file -
--- wsgi.py
+++ wsgi.py
@@ -2,5 +2,4 @@ from app import app
 import os
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 8000)))
